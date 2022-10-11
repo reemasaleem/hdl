f6fd8dc98fa8e9bc22d5e9e0e6496e2e21d659bf
Stanca Pop <stanca.pop@analog.com>
2022-10-11 17:36:07 +0300

    ad7616_bd.tcl: Fix irq consistency

--- projects/ad7616_sdz/common/ad7616_bd.tcl
+++ projects/ad7616_sdz/common/ad7616_bd.tcl
@@ -78,7 +78,8 @@ if {$SI_OR_PI == 0} {
 
   # interrupts
 
-  ad_cpu_interrupt ps-14 mb-14 /$hier_spi_engine/irq
+  ad_cpu_interrupt ps-12 mb-12 /$hier_spi_engine/irq
+
 } else {
   # data interfaces
 
@@ -116,7 +117,7 @@ if {$SI_OR_PI == 0} {
 
   # interrupts
 
-  ad_cpu_interrupt ps-12 mb-13 axi_ad7616/irq
+  ad_cpu_interrupt ps-10 mb-10 axi_ad7616/irq
 }
 
 # interface connections
@@ -139,4 +140,4 @@ ad_mem_hp1_interconnect sys_cpu_clk axi_ad7616_dma/m_dest_axi
 
 # interrupts
 
-ad_cpu_interrupt ps-13 mb-12 axi_ad7616_dma/irq
+ad_cpu_interrupt ps-13 mb-13 axi_ad7616_dma/irq
