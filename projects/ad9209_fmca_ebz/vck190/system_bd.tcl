a1e6952eb5a9f7504d14348fffe922b477f11d9a
Pop.Ioan-daniel <pioandan@romlx5.adlk.analog.com>
2023-07-05 14:18:12 +0300

    projects: Add missing sysid IP
    The sysid IP was added in ad9209_fmca_ebz/vck190, ad9213_dual_ebz/s10soc
    and ad9213_dual_ebz/s10soc
    
    Signed-off-by: Pop Ioan Daniel <pop.ioan-daniel@analog.com>

--- projects/ad9209_fmca_ebz/vck190/system_bd.tcl
+++ projects/ad9209_fmca_ebz/vck190/system_bd.tcl
@@ -11,3 +11,10 @@ set ad_project_params(TX_NUM_LINKS) 1
 set ad_project_params(TX_KS_PER_CHANNEL) 1
 
 source $ad_hdl_dir/projects/ad9081_fmca_ebz/vck190/system_bd.tcl
+
+#system ID
+ad_ip_parameter axi_sysid_0 CONFIG.ROM_ADDR_BITS 9
+ad_ip_parameter rom_sys_0 CONFIG.PATH_TO_FILE "[pwd]/mem_init_sys.txt"
+ad_ip_parameter rom_sys_0 CONFIG.ROM_ADDR_BITS 9
+set sys_cstring "sys rom custom string placeholder"
+sysid_gen_sys_init_file $sys_cstring
--- projects/ad9213_dual_ebz/s10soc/system_qsys.tcl
+++ projects/ad9213_dual_ebz/s10soc/system_qsys.tcl
@@ -1,6 +1,7 @@
 
 set adc_fifo_address_width 15
 
+source $ad_hdl_dir/projects/scripts/adi_pd.tcl
 source $ad_hdl_dir/projects/common/s10soc/s10soc_system_qsys.tcl
 source $ad_hdl_dir/projects/common/intel/adcfifo_qsys.tcl
 
@@ -9,3 +10,12 @@ if [info exists ad_project_dir] {
 } else {
   source ../common/ad9213_dual_qsys.tcl
 }
+
+#system ID
+set_instance_parameter_value axi_sysid_0 {ROM_ADDR_BITS} {9}
+set_instance_parameter_value rom_sys_0 {ROM_ADDR_BITS} {9}
+
+set_instance_parameter_value rom_sys_0 {PATH_TO_FILE} "[pwd]/mem_init_sys.txt"
+
+set sys_cstring "sys rom custom string placeholder";
+sysid_gen_sys_init_file $sys_cstring;
